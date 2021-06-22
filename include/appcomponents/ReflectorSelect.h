455e21a2f1b454e742a322c381d4d194896ab46c
Nicholas Hoang <nicholash489@gmail.com>
2021-06-21 23:37:28 -0500

    Added Reflectors List Initilization

--- include/appcomponents/ReflectorSelect.h
+++ include/appcomponents/ReflectorSelect.h
@@ -2,13 +2,14 @@
 #define REFLECTORSELECT_H
 
 #include <wx/wx.h>
+#include <vector>
 
 class ReflectorSelect : public wxComboBox
 {
     public:
         ReflectorSelect(wxWindow* parent, const wxPoint &pos, const wxSize &size);
         ~ReflectorSelect();
-        void loadReflectors();
+        void loadReflectors(std::vector<std::string> rotors);
 
     private:
         void OnChoose(wxCommandEvent& event);
--- src/ReflectorSelect.cpp
+++ src/ReflectorSelect.cpp
@@ -8,5 +8,17 @@ ReflectorSelect::ReflectorSelect(wxWindow* parent, const wxPoint &pos=wxDefaultP
 
 ReflectorSelect::~ReflectorSelect()
 {
+    
+}
 
+void ReflectorSelect::loadReflectors(std::vector<std::string> reflectors)
+{
+    std::vector<wxString> names;
+    names.resize(reflectors.size());
+    std::transform(reflectors.begin(), reflectors.end(), names.begin(), [](std::string str){
+        wxString name(str);
+        return name;
+    });
+    this->Set(names);
+    this->SetSelection(0);
 }
