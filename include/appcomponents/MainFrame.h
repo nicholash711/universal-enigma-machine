30a3a4572426655ca9d8f65217ce58374ba3792d
Nicholas Hoang <nicholash489@gmail.com>
2021-06-21 23:23:46 -0500

    Adding Features to Main Frame

--- include/appcomponents/MainFrame.h
+++ include/appcomponents/MainFrame.h
@@ -4,23 +4,34 @@
 #include <wx/wx.h>
 #include <wx/statline.h>
 #include <enigma/Enigma.h>
+#include <DataImport.h>
+#include <appcomponents/RotorSelect.h>
+#include <appcomponents/CharSpin.h>
 
 class MainFrame : public wxFrame
 { 
     public:
         MainFrame();
         ~MainFrame();
-        void loadComponents();
+        void loadComponents(std::string name);
 
     private:
         Enigma* enigma = nullptr;
+        DataImport* file = nullptr;
         wxTextCtrl* input = nullptr;
         wxTextCtrl* output = nullptr;
         wxStaticText* text = nullptr;
         wxStaticLine* line = nullptr;
+        std::string model = "";
+        RotorSelect* rotors[4];
+        CharSpin* spin1[4];
+        CharSpin* spin2[4];
+
+        void loadRotors(std::string name);
 
         void OnPress(wxCommandEvent& event);
         void PressFile(wxCommandEvent& event);
+        void OnModelChoose(wxCommandEvent& event);
 };
 
 #endif
--- src/MainFrame.cpp
+++ src/MainFrame.cpp
@@ -1,11 +1,13 @@
+#include <rapidjson/document.h>
 #include <appcomponents/MainFrame.h>
 #include <appcomponents/ModelSelect.h>
 #include <appcomponents/CharSpin.h>
 #include <appcomponents/RotorSelect.h>
 #include <appcomponents/PlugboardInput.h>
 #include <appcomponents/ReflectorSelect.h>
-#include <DataImport.h>
 #include <cstdio>
+
+namespace json = rapidjson;
 
 MainFrame::MainFrame() : 
     wxFrame(nullptr, wxID_ANY, "Enigma Machine", wxPoint(100, 100), wxSize(800, 600),  wxMINIMIZE_BOX | wxSYSTEM_MENU | wxCAPTION | wxCLOSE_BOX | wxCLIP_CHILDREN)
@@ -24,23 +26,22 @@ MainFrame::MainFrame() :
 
     // Model Menu Creation
     text = new wxStaticText(this, wxID_ANY, "Enigma Model: ", wxPoint(10, 12), wxSize(100, 20));
-    ModelSelect* menu = new ModelSelect(this, wxPoint(110, 10), wxSize(300, 22));
+    ModelSelect* menu = new ModelSelect(this, wxPoint(110, 10), wxSize(300, 24));
     line = new wxStaticLine(this, wxID_ANY, wxPoint(10, 45), wxSize(780, 1));
+
+    menu->Bind(wxEVT_COMBOBOX, OnModelChoose, this);
     
 
     // Rotor Controls Creation
-    RotorSelect* rotors = nullptr;
-    CharSpin* spin1 = nullptr;
-    CharSpin* spin2 = nullptr;
-    for(int i = 1; i < 5; i++)
+    for(int i = 0; i < 4; i++)
     {
-        const std::string rotor     = "Rotor " + std::to_string(i) + ":";
-        text = new wxStaticText(this, wxID_ANY, rotor, wxPoint(10, 22 + 40 * i), wxSize(50, 20));
-        rotors = new RotorSelect(this, wxPoint(60, 20 + 40 * i), wxSize(40, 20));
-        text = new wxStaticText(this, wxID_ANY, "Rotor Position:", wxPoint(120, 22 + 40 * i), wxSize(80, 20));
-        spin1 = new CharSpin(this, wxPoint(210, 20 + 40 * i), wxSize(40, 20));
-        text = new wxStaticText(this, wxID_ANY, "Ring Positon:", wxPoint(270, 22 + 40 * i), wxSize(80, 20));
-        spin2 = new CharSpin(this, wxPoint(350, 20 + 40 * i), wxSize(40, 20));
+        const std::string rotor = "Rotor " + std::to_string(i + 1) + ":";
+        text = new wxStaticText(this, wxID_ANY, std::string("Rotor " + std::to_string(i + 1) + ":"), wxPoint(10, 62 + 40 * i), wxSize(50, 20));
+        rotors[i] = new RotorSelect(this, wxPoint(60, 60 + 40 * i), wxSize(40, 20));
+        text = new wxStaticText(this, wxID_ANY, "Rotor Position:", wxPoint(120, 62 + 40 * i), wxSize(80, 20));
+        spin1[i] = new CharSpin(this, wxPoint(210, 60 + 40 * i), wxSize(40, 20));
+        text = new wxStaticText(this, wxID_ANY, "Ring Positon:", wxPoint(270, 62 + 40 * i), wxSize(80, 20));
+        spin2[i] = new CharSpin(this, wxPoint(350, 60 + 40 * i), wxSize(40, 20));
 
     }
 
@@ -49,7 +50,7 @@ MainFrame::MainFrame() :
     text = new wxStaticText(this, wxID_ANY, "Reflector:", wxPoint(410, 62), wxSize(60, 20));
     ReflectorSelect* reflectorSelect = new ReflectorSelect(this, wxPoint(470, 60), wxSize(320, 20));
     text = new wxStaticText(this, wxID_ANY, "Plugboard Setting:", wxPoint(410, 102), wxSize(110, 20));
-    PlugboardInput* plugboardInput = new PlugboardInput(this, wxPoint(520, 100), wxSize(270, 20));
+    PlugboardInput* plugboardInput = new PlugboardInput(this, wxPoint(520, 100), wxSize(270, 24));
     line = new wxStaticLine(this, wxID_ANY, wxPoint(10, 210), wxSize(780, 1));
 
 
@@ -63,13 +64,48 @@ MainFrame::MainFrame() :
     encode->Bind(wxEVT_BUTTON, OnPress, this);
 
     enigma = new Enigma();
-    DataImport* file = new DataImport("models.json");
-    std::vector<std::string> names = file->getModels();
-    menu->loadModels(file->getModels());
+    file = new DataImport("models.json");
+    menu->loadModels(file->getModelList());
 }
 
 MainFrame::~MainFrame()
 {
+}
+
+void MainFrame::loadComponents(std::string name)
+{
+    model = name;
+    loadRotors(name);
+}
+
+void MainFrame::loadRotors(std::string name)
+{
+    for(int i = 0; i < 4; i++)
+    {
+        rotors[i]->Clear();
+        spin1[i]->setText('A');
+        spin2[i]->setText('A');
+    }
+    if(file->hasFour(name))
+    {
+        rotors[3]->Enable(true);
+        spin1[3]->enable(true);
+        spin2[3]->enable(true);
+        for(int i = 1; i < 4; i++)
+            rotors[i]->loadChoices(file->getRotorList(name));
+        rotors[0]->loadChoices(file->getRotor4(name));
+    }
+    else
+    {
+        for(int i = 0; i < 3; i++)
+            rotors[i]->loadChoices(file->getRotorList(name));
+        rotors[3]->Enable(false);
+        spin1[3]->enable(false);
+        spin2[3]->enable(false);
+        
+    }
+        
+
 }
 
 void MainFrame::OnPress(wxCommandEvent& event)
@@ -79,5 +115,12 @@ void MainFrame::OnPress(wxCommandEvent& event)
 
 void MainFrame::PressFile(wxCommandEvent& event)
 {
-    std::cout << FindFocus() << std::endl;  
+    std::cout << "File Pressed" << std::endl;  
+}
+
+void MainFrame::OnModelChoose(wxCommandEvent& event)
+{
+    ModelSelect* menu = (ModelSelect*)event.GetEventObject();
+    if(model != std::string(menu->GetValue()))
+        loadComponents(std::string(menu->GetValue()));
 }
