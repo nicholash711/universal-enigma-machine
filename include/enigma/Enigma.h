3353ca908947209b4e758fb7b824010156674de6
Nicholas Hoang <nicholash489@gmail.com>
2021-06-22 04:49:29 -0500

    Changed members to pointers; Added Access Methods

--- include/enigma/Enigma.h
+++ include/enigma/Enigma.h
@@ -10,23 +10,27 @@
 class Enigma
 {
     private:
-        Plugboard plugboard;
-        std::array<Rotor, 3> rotors;
-        Reflector reflector;
-        EntryWheel entry;
+        std::array<Rotor*, 3> rotors;
+        Rotor* rotor4;
+        Plugboard* plugboard;
+        Reflector* reflector;
+        EntryWheel* entry;
+
         char rotorEncryption(char c);
         void rotateRotors();
         char encryptChar(char c);
+
     public:
         Enigma();
-        std::array<Rotor, 3> getRotors();
-        Reflector getReflectors();
-        Plugboard getPlugboard();
+        std::array<Rotor*, 3> getRotors();
+        Reflector* getReflector();
+        Plugboard* getPlugboard();
+        EntryWheel* getEntryWheel();
         std::string encrypt(std::string mes);
-        bool setRotor(Rotor rot, int pos);
+        void setRotor(Rotor& rot, int pos);
         bool setPlug(std::string plug);
-        void setReflector(Reflector ref);
-        void setEntryWheel(EntryWheel entry);
+        void setReflector(Reflector& ref);
+        void setEntryWheel(EntryWheel& entry);
         bool setPosition(char c, int pos);
         bool setRing(char c, int pos);
         bool clearRotor(int i);
--- src/Enigma.cpp
+++ src/Enigma.cpp
@@ -3,36 +3,55 @@
 
 Enigma::Enigma()
 {
-    
+    rotors.fill(nullptr);
+    rotor4 = nullptr;
+    plugboard = new Plugboard();
+    reflector = nullptr;
+    entry = nullptr;
 }
 
 char Enigma::rotorEncryption(char c)
 {
-    int encrypted = rotors[0].encrypt(rotors[1].encrypt(rotors[2].encrypt(c)));
-    encrypted = reflector.reflect(encrypted);
-    encrypted = rotors[2].reencrypt(rotors[1].reencrypt(rotors[0].reencrypt(encrypted)));
+    int encrypted = rotors[0]->encrypt(rotors[1]->encrypt(rotors[2]->encrypt(c)));
+    encrypted = reflector->reflect(encrypted);
+    encrypted = rotors[2]->reencrypt(rotors[1]->reencrypt(rotors[0]->reencrypt(encrypted)));
     return encrypted + 65;
 
 }
 void Enigma::rotateRotors()
 {
-    if(rotors[2].rotate())
-        if(rotors[1].rotate())
-            rotors[0].rotate();
+    if(rotors[2]->rotate())
+        if(rotors[1]->rotate())
+            rotors[0]->rotate();
 }
 
-std::array<Rotor, 3> Enigma::getRotors()
+std::array<Rotor*, 3> Enigma::getRotors()
 {
     return rotors;
+}
+
+Reflector* Enigma::getReflector()
+{
+    return reflector;
+}
+
+Plugboard* Enigma::getPlugboard()
+{
+    return plugboard;
+}
+
+EntryWheel* Enigma::getEntryWheel()
+{
+    return entry;
 }
 
 char Enigma::encryptChar(char c)
 {
     rotateRotors();
-    char encrypted = entry.enterChar(c);
-    encrypted = plugboard.encrypt(c);
+    char encrypted = entry->enterChar(c);
+    encrypted = plugboard->encrypt(c);
     encrypted = rotorEncryption(encrypted);
-    encrypted = plugboard.encrypt(encrypted);
+    encrypted = plugboard->encrypt(encrypted);
     
     return encrypted;
 }
@@ -53,55 +72,50 @@ std::string Enigma::encrypt(std::string mes)
     return encrypted;
 }
 
-bool Enigma::setRotor(Rotor rot, int i)
+void Enigma::setRotor(Rotor& rot, int i)
 {
-    for(Rotor rotor: rotors)
-        if(rotor == rot)
-            return false;
-    rotors[i - 1] = rot;
-    return true;
+    rotors[i - 1] = new Rotor(rot);
 }
 
 bool Enigma::setPlug(std::string plug)
 {
-    return plugboard.addPlug(plug);
+    return plugboard->addPlug(plug);
 }
 
-void Enigma::setReflector(Reflector ref)
+void Enigma::setReflector(Reflector& ref)
 {
-    reflector = ref;
+    reflector = new Reflector(ref);
 }
 
-void Enigma::setEntryWheel(EntryWheel entry)
+void Enigma::setEntryWheel(EntryWheel& entry)
 {
-    this->entry = entry;
+    this->entry = new EntryWheel(entry);
 }
 
 bool Enigma::setPosition(char c, int i)
 {
-    return rotors[i - 1].setRotorPosition(c);
+    return rotors[i - 1]->setRotorPosition(c);
 }
 
 bool Enigma::setRing(char c, int i)
 {
-    return rotors[i - 1].setRingSetting(c);
+    return rotors[i - 1]->setRingSetting(c);
 }
 
 bool Enigma::clearRotor(int i)
 {
-    Rotor empty;
     if(i < 1 || i > 3)
         return false;
-    rotors[i - 1] = empty;
+    rotors[i - 1] = new Rotor();
     return true;
 }
 
 std::ostream& operator<<(std::ostream &out, Enigma &enigma)
 {
     out << "Plugboard: " << enigma.plugboard <<"\n";
-    out << "Rotors: " << enigma.rotors[0].getName() << " " << enigma.rotors[1].getName() << " " << enigma.rotors[2].getName() << "\n";
-    out << "Rotor Settings: " << enigma.rotors[0].getCurrent() << " " << enigma.rotors[1].getCurrent() << " " << enigma.rotors[2].getCurrent() << "\n";
-    out << "Ring Settings: " << enigma.rotors[0].getRing() << " " << enigma.rotors[1].getRing() << " " << enigma.rotors[2].getRing() << "\n";
-    out << "Reflector: " << enigma.reflector.getName();
+    out << "Rotors: " << enigma.rotors[0]->getName() << " " << enigma.rotors[1]->getName() << " " << enigma.rotors[2]->getName() << "\n";
+    out << "Rotor Settings: " << enigma.rotors[0]->getCurrent() << " " << enigma.rotors[1]->getCurrent() << " " << enigma.rotors[2]->getCurrent() << "\n";
+    out << "Ring Settings: " << enigma.rotors[0]->getRing() << " " << enigma.rotors[1]->getRing() << " " << enigma.rotors[2]->getRing() << "\n";
+    out << "Reflector: " << enigma.reflector->getName();
     return out;
 }
